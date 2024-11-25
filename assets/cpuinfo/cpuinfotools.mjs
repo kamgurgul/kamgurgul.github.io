d2ac650037add29e6515ee293c8f8f2b6d087ad5
Kamil Gurgul <kamgurgul@gmail.com>
2024-11-25 08:33:03 +0100

    Add cpuinfo app

---
+++ assets/cpuinfo/cpuinfotools.mjs
@@ -0,0 +1,41 @@
+function getUsedHeapSize() {
+    if (performance && performance.memory) {
+        return performance.memory.usedJSHeapSize;
+    } else {
+        return -1;
+    }
+}
+
+function getTotalHeapSize() {
+    if (performance && performance.memory) {
+        return performance.memory.totalJSHeapSize;
+    } else {
+        return -1;
+    }
+}
+
+async function getTotalStorage() {
+    if ("storage" in navigator && "estimate" in navigator.storage) {
+        const { usage, quota } = await navigator.storage.estimate();
+        return BigInt(quota);
+    } else {
+        return BigInt(-1);
+    }
+}
+
+async function getUsedStorage() {
+    if ("storage" in navigator && "estimate" in navigator.storage) {
+        const { usage, quota } = await navigator.storage.estimate();
+        return BigInt(usage);
+    } else {
+        return BigInt(-1);
+    }
+}
+
+function isPdfViewerEnabled() {
+    if ("pdfViewerEnabled" in navigator) {
+        return navigator.pdfViewerEnabled;
+    } else {
+        return false;
+    }
+}
